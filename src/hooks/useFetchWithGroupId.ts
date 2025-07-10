5571487f86b96e8af9ac909e14e37a46bc46f297
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-07-10 14:20:01 +0200

    coorecting order creation

--- src/hooks/useFetchWithGroupId.ts
+++ src/hooks/useFetchWithGroupId.ts
@@ -2,19 +2,22 @@ import { useAppSelector } from "./redux";
 
 export const useFetchWithGroupId = () => {
     const groupId = useAppSelector(state => state.login.groupId);
-  
-    const fetchWithGroup = async (url: string, options = {}) => {
-      const headers = {
-        "group-Id": groupId,
-      };
-  
-      const finalOptions = {
-        ...options,
-        headers,
-      };
-  
-      return fetch(url, finalOptions);
+
+    const fetchWithGroup = async (url: string, options: RequestInit = {}) => {
+        const headers = {
+            "group-Id": groupId,
+            ...(options.headers || {}),
+        };
+
+
+        const finalOptions = {
+            ...options,
+            headers,
+        };
+
+
+        return fetch(url, finalOptions);
     };
-  
+
     return fetchWithGroup;
-  };
+};
