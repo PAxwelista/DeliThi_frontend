175682077647ea2786edad8fc5370c45a6d8e373
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-10-29 16:52:42 +0100

    fix: correct with the new InputType

--- src/screens/Mores/DetailCustomerScreen.tsx
+++ src/screens/Mores/DetailCustomerScreen.tsx
@@ -1,4 +1,4 @@
-import { Screen, InputFormForUpdate } from "../../components/";
+import { Screen, InputFormForUpdate, InputType } from "../../components/";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { MoreMenuStackParamList, CustomerForm } from "../../types";
 import { useFetchWithAuth } from "../../hooks";
@@ -7,19 +7,19 @@ import { useState } from "react";
 
 type Props = NativeStackScreenProps<MoreMenuStackParamList, "DetailCustomer">;
 
-const inputs: Record<keyof CustomerForm, string> = {
-    name: "Nom",
-    locationName: "Lieu",
-    area: "Zone",
-    email: "Mail",
-    phoneNumber: "Numéro de téléphone",
+const inputs: Record<keyof CustomerForm, InputType> = {
+    name: { value: "Nom", keyboardType: "default" },
+    locationName: { value: "Lieu", keyboardType: "default" },
+    area: { value: "Zone", keyboardType: "default" },
+    email: { value: "Mail", keyboardType: "default" },
+    phoneNumber: { value: "Numéro de téléphone", keyboardType: "default" },
 };
 
 function DetailCustomerScreen({ route }: Props) {
     const fetchWithAuth = useFetchWithAuth();
     const { name, email, phoneNumber } = route.params;
     const { name: locationName, area } = route.params.location;
-    const [values, setValues] = useState<typeof inputs>({
+    const [values, setValues] = useState<Record<keyof typeof inputs, string>>({
         name,
         locationName,
         area,
@@ -33,7 +33,7 @@ function DetailCustomerScreen({ route }: Props) {
             .map(([key, value]) => key + "=" + value)
             .join("&");
 
-            fetchWithAuth(`${apiUrl}/Customers/${route.params._id}?${urlRequest}`, {
+        fetchWithAuth(`${apiUrl}/Customers/${route.params._id}?${urlRequest}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
